f0a2cc77f30958fec27844d613bcfeb5ff38f2d6
Shimon <shimonbhuiya02@gmail.com>
2023-11-13 21:57:57 +0600

    Home page done

--- app/src/main/java/com/example/shimon/quizappintern/LoginFragment.kt
+++ app/src/main/java/com/example/shimon/quizappintern/LoginFragment.kt
@@ -1,13 +1,16 @@
 package com.example.shimon.quizappintern
 
 import android.os.Bundle
+import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.shimon.quizappintern.base.BaseFragment
 import com.example.shimon.quizappintern.databinding.FragmentLoginBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::inflate) {
@@ -18,7 +21,18 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
 
         binding.loginBTN.setOnClickListener {
 
-            findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            var email = binding.emailET.text.toString().trim()
+            var password = binding.passET.text.toString().trim()
+            
+            if (isEmailValid(email)&&isPasswordValid(password)){
+
+                loginUser(email,password)
+            }else
+            {
+                Toast.makeText(requireContext(), "Invalid Email and Password", Toast.LENGTH_SHORT).show()
+            }
+
+
 
         }
 
@@ -28,6 +42,34 @@ class LoginFragment : BaseFragment<FragmentLoginBinding>(FragmentLoginBinding::i
 
         }
         
+    }
+
+    private fun loginUser(email: String, password: String) {
+
+        val auth = FirebaseAuth.getInstance()
+
+        auth.signInWithEmailAndPassword(email,password).addOnCompleteListener {task->
+            if (task.isSuccessful){
+                val user = auth.currentUser
+                Toast.makeText(requireContext(), "Login SuccessFully\n ${user?.email}", Toast.LENGTH_SHORT).show()
+                findNavController().navigate(R.id.action_loginFragment_to_homeFragment)
+            }
+            else{
+                Toast.makeText(requireContext(),"Login Failed ${task.exception?.message}",Toast.LENGTH_SHORT).show()
+            }
+
+        }
+
+    }
+
+    fun isEmailValid(email: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+
+    fun isPasswordValid(password: String): Boolean {
+//        val passwordRegex =
+//            Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$")
+        return password.length >= 6
     }
 
 }
